8e3b8795e027bb004fea5c4e67aeb7255aac3f8b
alejandro <alejandro@debian.alejandro>
2026-06-16 08:18:30 -0500

    feat(music):definir interfas

---
+++ src/features/music/domain/MusicRepository.ts
@@ -0,0 +1,21 @@
+import { Track } from './models/Track';
+
+export interface MusicRepository {
+  /**
+   * Busca canciones en la plataforma (YouTube Music / Servidor)
+   * @param query Término de búsqueda (Nombre de canción, artista)
+   */
+  searchTracks(query: string): Promise<Track[]>;
+
+  /**
+   * Obtiene la URL de streaming de alta calidad de forma segura
+   * @param trackId ID de la canción a reproducir
+   */
+  getStreamUrl(trackId: string): Promise<string>;
+
+  /**
+   * Obtiene las letras de una canción si están disponibles
+   * @param trackId ID de la canción
+   */
+  getLyrics?(trackId: string): Promise<string>;
+}
